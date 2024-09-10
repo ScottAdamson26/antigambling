7fbac183a1941c7c692547c7d5a6ed9309d05051
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2024-09-10 22:10:12 +0100

    update

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Feature from './Feature';
 import Showcase from './Showcase';
 import Blogs from './Blogs';
-
+import Videos from './Videos';
 function HomePage() {
   return (
     <div className="w-full mx-auto bg-background">
@@ -10,6 +10,7 @@ function HomePage() {
       <Showcase />
       
       <Blogs />
+      <Videos />
     </div>
   );
 }
