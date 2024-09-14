f9250e54d5c712ea7ed2700ca9c867068b263960
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2024-09-14 16:11:13 +0100

    update

--- netlify/functions/discord-login.js
+++ netlify/functions/discord-login.js
@@ -1,8 +1,8 @@
 exports.handler = async function (event, context) {
     const clientId = process.env.DISCORD_CLIENT_ID;
-    const redirectUri = process.env.DISCORD_REDIRECT_URI;
+    const redirectUri = process.env.DISCORD_REDIRECT_URI || "https://antigambling.netlify.app/.netlify/functions/discord-callback";
   
-    const discordLoginUrl = `https://discord.com/oauth2/authorize?client_id=1283353891815161938&response_type=code&redirect_uri=https%3A%2F%2Fantigambling.netlify.app%2Fauth%2Fdiscord%2Fcallback&scope=identify`;
+    const discordLoginUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify`;
   
     return {
       statusCode: 302,
