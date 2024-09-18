a7bfc62af0b7a4b725a01cf412aa5d2a6200e6c6
ScottAdamson26 <44335395+ScottAdamson26@users.noreply.github.com>
2024-09-18 09:59:35 +0100

    update

--- src/Profile.js
+++ src/Profile.js
@@ -9,12 +9,14 @@ function Profile() {
   }
 
   return (
-    <div className="profile-page">
-      <h1>Welcome, {username}</h1>
-      <img
+    <div className="profile-page bg-navBg flex flex-col items-center p-10 shadow-lg">
+          <img
         src={`https://cdn.discordapp.com/avatars/${id}/${avatar}.png`}
-        alt={`${username}'s avatar`}
+        alt={`${username}'s avatar`} 
+        className='rounded-full w-64 h-64 order-solid border-2 border-lightOrange'
       />
+      <h1 className='text-white font-semibold text-xl mt-6'>{username}</h1>
+    
     </div>
   );
 }
